d36079e44f897db0e243394f3ee32c58d2f53f9e
Nick Fed <fna.21@uni-dubna.ru>
2023-11-27 17:44:50 +0900

    mutliprocessing

--- calculate_similar_libraries.py
+++ calculate_similar_libraries.py
@@ -1,59 +1,130 @@
 import json, csv
+from functools import partial
+from itertools import repeat
+from multiprocessing import Pool, freeze_support
+import numpy as np
+import itertools
 
-with open("imported_libraries.json", "r") as f:
-    data = json.load(f)
+def calculate_libraries(soft_list, full_list):
+    result = list()
 
-software_list = data["software"]
+    for software in soft_list:
+        file1 = software["filename"]
+        file1_type = software["type"]
+        file1_path = software["filepath"]
+        imports_list1 = software["imports"]
+        unique_libraries1 = software["unique_libraries"]
 
-#output = "file1\tfile2\timports %\tunique libraries %\n\n"
+        for comparison in full_list:
+            if (software is comparison): continue
 
-header = ["file1", "file1_type", "file2", "file2_type", "imports, %",
-           "unique libraries, %", "file1_path", "file2_path"]
+            file2 = comparison["filename"]
+            file2_type = comparison["type"]
+            file2_path = comparison["filepath"]
+            imports_list2 = comparison["imports"]
+            unique_libraries2 = comparison["unique_libraries"]
 
-with open("lib_similarity.csv", "w", encoding="UTF8", newline="") as f:
-    writer = csv.writer(f)
-    writer.writerow(header)
+            if len(imports_list1) != 0:
+                same_imports_perc = len(set(imports_list1) &
+                                         set(imports_list2)) / len(imports_list1) * 100
+            else:
+                same_imports_perc = 0
 
-for software in software_list:
-    file1 = software["filename"]
-    file1_type = software["type"]
-    file1_path = software["filepath"]
-    imports_list1 = software["imports"]
-    unique_libraries1 = software["unique_libraries"]
+            if  len(unique_libraries1) != 0:
+                same_uniques_perc = len(set(unique_libraries1) &
+                                         set(unique_libraries2)) / len(unique_libraries1) * 100
+            else:
+                same_uniques_perc = 0
 
-    for comparison in software_list:
-        if (software is comparison): continue
+            #if file1_type != file2_type and same_imports_perc < 70 or same_uniques_perc < 65: continue
+            #if file1_type != file2_type and same_imports_perc < 70: continue
+            #if file1_type == file2_type and same_imports_perc > 30 or same_uniques_perc > 35: continue
+            #if file1_type == file2_type and same_imports_perc > 30: continue
 
-        file2 = comparison["filename"]
-        file2_type = comparison["type"]
-        file2_path = comparison["filepath"]
-        imports_list2 = comparison["imports"]
-        unique_libraries2 = comparison["unique_libraries"]
-
-        if len(imports_list1) != 0:
-            same_imports_perc = len(set(imports_list1) &
-                                     set(imports_list2)) / len(imports_list1) * 100
-        else:
-            same_imports_perc = 0
-
-        if  len(unique_libraries1) != 0:
-            same_uniques_perc = len(set(unique_libraries1) &
-                                     set(unique_libraries2)) / len(unique_libraries1) * 100
-        else:
-            same_uniques_perc = 0
-
-        #if (same_imports_perc == 0 and same_uniques_perc <= 50): continue
-
-        #newline = "\t".join((file1, file2, str(same_imports_perc), str(same_uniques_perc), "\n"))
-        #output += newline
-
-        if file1_type != file2_type and same_imports_perc < 70 or same_uniques_perc < 65: continue
-        if file1_type == file2_type and same_imports_perc > 30 or same_uniques_perc > 35: continue
-
-        with open("lib_similarity.csv", "a", encoding="UTF8", newline="") as f:
-            writer = csv.writer(f)
-            writer.writerow([file1, file1_type, file2, file2_type, 
+            result.append([file1, file1_type, file2, file2_type, 
                              same_imports_perc, same_uniques_perc, file1_path, file2_path])
+
+    return result
+
+def main():
+    with open("imported_libraries.json", "r") as f:
+        data = json.load(f)
+
+    software_list = data["software"]
+
+    header = ["file1", "file1_type", "file2", "file2_type", "imports, %",
+               "unique libraries, %", "file1_path", "file2_path"]
+
+    soft_lists = np.array_split(software_list, 3)
+
+    with Pool() as pool:
+        result = pool.starmap(calculate_libraries,
+                                                  zip(soft_lists, repeat(software_list)))
+
+    result = list(itertools.chain.from_iterable(result))
+
+    with open("lib_similarity.csv", "w", encoding="UTF8", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(header)
+        for res in result:
+            writer.writerow(res)
+
+    #with Pool() as pool:
+    #    L = pool.starmap(calculate_libraries, [(1, 1), (2, 1), (3, 1)])
+    #    M = pool.starmap(calculate_libraries, zip(a_args, repeat(second_arg)))
+    #    N = pool.map(partial(calculate_libraries, b=second_arg), a_args)
+    #    assert L == M == N
+
+if __name__ == "__main__":
+    freeze_support()
+    main()
+
+
+
+
+
+#for software in software_list:
+#    file1 = software["filename"]
+#    file1_type = software["type"]
+#    file1_path = software["filepath"]
+#    imports_list1 = software["imports"]
+#    unique_libraries1 = software["unique_libraries"]
+#
+#    for comparison in software_list:
+#        if (software is comparison): continue
+#
+#        file2 = comparison["filename"]
+#        file2_type = comparison["type"]
+#        file2_path = comparison["filepath"]
+#        imports_list2 = comparison["imports"]
+#        unique_libraries2 = comparison["unique_libraries"]
+#
+#        if len(imports_list1) != 0:
+#            same_imports_perc = len(set(imports_list1) &
+#                                     set(imports_list2)) / len(imports_list1) * 100
+#        else:
+#            same_imports_perc = 0
+#
+#        if  len(unique_libraries1) != 0:
+#            same_uniques_perc = len(set(unique_libraries1) &
+#                                     set(unique_libraries2)) / len(unique_libraries1) * 100
+#        else:
+#            same_uniques_perc = 0
+#
+#        #if (same_imports_perc == 0 and same_uniques_perc <= 50): continue
+#
+#        #newline = "\t".join((file1, file2, str(same_imports_perc), str(same_uniques_perc), "\n"))
+#        #output += newline
+#
+#        #if file1_type != file2_type and same_imports_perc < 70 or same_uniques_perc < 65: continue
+#        if file1_type != file2_type and same_imports_perc < 70: continue
+#        #if file1_type == file2_type and same_imports_perc > 30 or same_uniques_perc > 35: continue
+#        if file1_type == file2_type and same_imports_perc > 30: continue
+#
+#        with open("lib_similarity.csv", "a", encoding="UTF8", newline="") as f:
+#            writer = csv.writer(f)
+#            writer.writerow([file1, file1_type, file2, file2_type, 
+#                             same_imports_perc, same_uniques_perc, file1_path, file2_path])
 
 
 #with open("lib_similarity_list.txt", "w") as f:
