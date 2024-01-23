afbeb61b3248ab190d5653f9b449893412719e6b
Nick Fed <fna.21@uni-dubna.ru>
2024-01-23 11:31:42 +0900

    fixed the output to csv

--- pochi_scripts/extract-compare_all.groovy
+++ pochi_scripts/extract-compare_all.groovy
@@ -1,6 +1,7 @@
 import jp.cafebabe.birthmarks.comparators.Threshold;
 import jp.cafebabe.birthmarks.entities.Birthmarks;
 
+
 def birthmarkList = ["1-gram", "2-gram", "3-gram", "4-gram", "5-gram", "6-gram", "fuc", "uc"] //vom doesnt work 
 def comparatorList = ["Cosine", "DiceIndex", "EditDistance", "JaccardIndex", "SimpsonIndex"]
 def matcherList = ["Guessed", "RoundRobin", "RoundRobinWithSamePair", "Specified"]
@@ -22,12 +23,9 @@ def writeToFile(matchResult, filename, directoryName){
 
 threshold = Threshold.DEFAULT // default threshold (0.75)
 
-header = ["birthmark", "comparator", "matcher", "file1", "file2", "result"]
-
-data = []
-data.append(header)
-
-//update to write all results to a single csv 
+def header = ["birthmark", "comparator", "matcher", "file1", "file2", "result"]
+def data = []
+data.add(header)
 
 for (currentBirthmark in birthmarkList){
     extractor = pochi.extractor(currentBirthmark)
@@ -44,21 +42,24 @@ for (currentBirthmark in birthmarkList){
         for (currentMatcher in matcherList){
             matcher = pochi.matcher(currentMatcher)
             
-            matchResult = matcher.match(birthmarks_1, birthmarks_2)
+            def result_lines = []
+            matcher.match(birthmarks_1, birthmarks_2)
                 .map(pair -> comparator.compare(pair))
                 .filter(either -> either.isRight())
                 .map(either -> either.get())
                 .filter(comparison -> comparison.isStolen(threshold))
+                .forEach(comparison -> result_lines.add(comparison))
 
             newline = [currentBirthmark, currentComparator, currentMatcher]
-            newline.merge(matchResult)
-            data.append(newline)
+
+            for (result in result_lines){
+                data.add(newline + result)
+            }
         }
     }
 }
 
 filename = args[0].split("/")[-1] + "_" +args[1].split("/")[-1] + ".csv"
-def file = File.createTempFile(filename, ".csv")
-file.withWriter { w ->
-    new CSVWriter(w).writeAll(data.collect{ it as String[] })
-}
+def file = new File(filename)
+
+file.text = data*.join(",").join(System.lineSeparator())
