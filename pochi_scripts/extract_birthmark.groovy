caa374fbf6199af8b2740d65155e452d68312458
Nick Fed <fna.21@uni-dubna.ru>
2024-02-10 18:00:41 +0900

    script for single birthmark extraction

---
+++ pochi_scripts/extract_birthmark.groovy
@@ -0,0 +1,29 @@
+import jp.cafebabe.birthmarks.comparators.Threshold;
+import jp.cafebabe.birthmarks.entities.Birthmarks;
+
+def extract(path, extractor) {
+    source = pochi.source(path)
+    return extractor.extract(source)
+}
+
+file_output_flag = false
+
+currentBirthmark = args[0]
+
+extractor = pochi.extractor(currentBirthmark)
+
+birthmarks = Arrays.stream(args[1])
+    .map(file -> extract(file, extractor))
+    .reduce(new Birthmarks(), (b1, b2) -> b1.merge(b2))
+
+birthmarks.forEach(birthmark -> println(birthmark))
+
+if (file_output_flag){
+    def data = []
+    birthmarks.forEach(birthmark -> data.add(birthmark))
+
+    directoryName = "birthmarks/"
+    filename = args[0].split("/")[-1] + "_" +args[1].split("/")[-1] + ".csv"
+    def file = new File(directoryName + filename)
+    file.text = data*.join(",").join(System.lineSeparator())
+}
