15165ecd039446907db7bff84e5d6311a88f6d7f
Nick Fed <fna.21@uni-dubna.ru>
2024-02-18 17:19:28 +0900

    updated the imports. removed redundant functions

--- extract_files_external_deps.py
+++ extract_files_external_deps.py
@@ -1,29 +1,13 @@
-import csv
 import os
 import sys
 
 import pandas as pd
 
+import csv_manager as cm
+import project_inspector as pi
+
 PROJECTS_DEP_FILE = "projects_dependencies.csv"
 OUTPUT_FILENAME = "files_dependencies.csv"
-
-
-def init_output_csv(header, filename):
-    with open(filename, "w", encoding="UTF8", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(header)
-
-
-def append_new_entry(filename, entry):
-    with open(filename, "a", encoding="UTF8", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(entry)
-
-
-def get_project_external_deps(dep_filename, project_name):
-    df = pd.read_csv(dep_filename)
-    external_deps = df.loc[df["project"] == project_name]["dependency"].tolist()
-    return external_deps
 
 
 def get_project_type(dep_filename, project_name):
@@ -35,37 +19,9 @@ def get_project_type(dep_filename, project_name):
     return type
 
 
-def get_projects_path_list(target_dir):
-    project_list = []
-    dirs_flag = True
-    for root, dirs, files in os.walk(target_dir):
-        depth = root[len(target_dir) + len(os.path.sep) :].count(os.path.sep)
-        if depth == 0:
-            if dirs_flag:
-                dirs_flag = False  # skip first entry that contains names of the directories from upper level
-                continue
-            for dir in dirs:
-                project_path = os.path.join(root, dir).replace("\\", "/")
-                project_list.append(project_path)
-        if depth != 0:
-            continue
-    return project_list
-
-
-def get_projects_filelist(project_path):
-    #TODO: add version as a second input
-    filelist = []
-    for root, dirs, files in os.walk(project_path):
-        for file in files:
-            if file.endswith(".java"):
-                filepath = os.path.join(root, file).replace("\\", "/")
-                filelist.append(filepath)
-    return filelist
-
-
 def extract_files_external_deps(filepath, project_path, dep_filename):
     project_name = os.path.basename(project_path)
-    project_deps = get_project_external_deps(dep_filename, project_name)
+    project_deps = pi.get_project_external_deps(dep_filename, project_name)
     external_deps = []
 
     with open(filepath, "r", errors="replace") as f:
@@ -92,13 +48,13 @@ def main():
 
     header = ["filename", "type", "project", "dependencies", "filepath"]
 
-    init_output_csv(header, OUTPUT_FILENAME)
+    cm.init_output_csv(header, OUTPUT_FILENAME)
 
-    project_list = get_projects_path_list(target_dir)
+    project_list = pi.get_projects_path_list(target_dir)
 
     for project in project_list:
-        #TODO: add function to get the list of versions
-        project_files = get_projects_filelist(project)
+        # TODO: add function to get the list of versions
+        project_files = pi.get_projects_filelist(project)
         for file in project_files:
             filename = os.path.basename(file)
             filepath = file
@@ -110,7 +66,7 @@ def main():
             if len(dependencies) == 0:
                 continue
             new_entry = [filename, project_type, project_name, dependencies, filepath]
-            append_new_entry(OUTPUT_FILENAME, new_entry)
+            cm.append_new_entry(OUTPUT_FILENAME, new_entry)
 
 
 if __name__ == "__main__":
