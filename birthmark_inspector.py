13bf26ae896be0c67af972e7577018cc7406c98f
nikfed <fedorov.nikolay.1110@gmail.com>
2024-03-01 12:34:29 +0900

    moved group info calculation to a separate function for birthmark inspector

--- birthmark_inspector.py
+++ birthmark_inspector.py
@@ -5,7 +5,7 @@ BIRTHMARK_DATA_FILE = "Cypher-Notepad-master_ChatGPT-1.0.2.csv"
 OUTPUT_FILENAME = "groupby_info"
 
 
-def main():
+def caluclate_group_info():
     df = pd.read_csv(BIRTHMARK_DATA_DIR + BIRTHMARK_DATA_FILE)
     df = df.loc[df.similarity == 1]
     df = df[
@@ -24,6 +24,10 @@ def main():
 
     with open(OUTPUT_FILENAME, mode="w") as f:
         f.write(groupby_info.to_string())
+
+
+def main():
+    caluclate_group_info()
 
 
 if __name__ == "__main__":
