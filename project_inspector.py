aa1c51992c8b53015761ffbd0b901ed0cc0a4379
Nick Fed <fna.21@uni-dubna.ru>
2024-03-02 20:48:00 +0900

    updated stopwords

--- project_inspector.py
+++ project_inspector.py
@@ -15,7 +15,7 @@ PROJECT_TYPES = [
     "/ebook_manager/",
 ]
 
-JAR_STOPWORDS = ["src", "lib", ".mvn", "dist"]
+JAR_STOPWORDS = ["src", "lib", ".mvn", "dist", "gradle"]
 
 
 def get_full_jar_list(dir):
