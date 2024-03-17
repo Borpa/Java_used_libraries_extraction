93e8a98ccc610e1620105cd977dd646997f9f862
Nick Fed <fna.21@uni-dubna.ru>
2024-03-17 20:40:06 +0900

    new function for unifying header format in output

--- birthmark_inspector.py
+++ birthmark_inspector.py
@@ -1,10 +1,71 @@
 import pandas as pd
 import os
 
+import project_inspector as pi
+from run_pochi import POCHI_OUTPUT_HEADER
+
 BIRTHMARK_DATA_DIR = "./birthmarks/"
 GROUPBY_OUTPUT_FILENAME = "groupby_info"
 AVG_SIMILARITY_OUTPUT_FILENAME = "avg_similarity"
 OUTPUT_DIR = "./birthmarks_group_data/"
+
+
+def full_column_names_check(file):
+    chunksize = 1000000
+    for chunk in pd.read_csv(file, chunksize=chunksize):
+        print(chunk.head(10))
+        print(chunk.columns)
+        return "project1" in chunk.columns.to_list()
+
+
+def update_columns(file):
+    chunksize = 100000
+    output_filename = file.replace(".csv", "_new_header.csv")
+
+    #with open(output_filename, "w") as f:
+    #    f.write(",".join(POCHI_OUTPUT_HEADER) + "\n")
+
+    with open(output_filename, "a") as f:
+        for chunk in pd.read_csv(file, chunksize=chunksize):
+            chunk = chunk[chunk.file1 != "file1"]
+
+            chunk["project1"] = chunk.apply(
+                lambda x: pi.get_project_name(x["file1"]), axis=1
+            )
+            chunk["project2"] = chunk.apply(
+                lambda x: pi.get_project_name(x["file2"]), axis=1
+            )
+            chunk["project1_ver"] = chunk.apply(
+                lambda x: pi.get_project_ver(x["file1"], x["project1"]), axis=1
+            )
+            chunk["project2_ver"] = chunk.apply(
+                lambda x: pi.get_project_ver(x["file2"], x["project2"]), axis=1
+            )
+            chunk["project1_file"] = chunk.apply(
+                lambda x: os.path.basename(x["file1"]), axis=1
+            )
+            chunk["project2_file"] = chunk.apply(
+                lambda x: os.path.basename(x["file2"]), axis=1
+            )
+            chunk["birthmark"] = chunk["currentBirthmark"]
+            chunk["comparator"] = chunk["currentComparator"]
+            chunk["matcher"] = chunk["currentMatcher"]
+            chunk["class1"] = chunk["class1"]
+            chunk["class2"] = chunk["class2"]
+            chunk["similarity"] = chunk["similarity"]
+
+            chunk = chunk.drop(
+                [
+                    "file1",
+                    "file2",
+                    "currentBirthmark",
+                    "currentMatcher",
+                    "currentComparator",
+                ],
+                axis=1,
+            )
+
+            chunk.to_csv(f, index=False)
 
 
 def retrieve_group_info(dataframe, output_filename=GROUPBY_OUTPUT_FILENAME):
@@ -87,8 +148,13 @@ def main():
         for file in os.listdir(OUTPUT_DIR):
             os.remove(OUTPUT_DIR + file)
 
-    birthmark_files = os.listdir(BIRTHMARK_DATA_DIR)
+    birthmark_dir = "G:/Study/phd_research/birthmarks/"
+
+    birthmark_files = os.listdir(birthmark_dir)
     for file in birthmark_files:
+        if not file.endswith(".csv"):
+            continue
+
         similarity_output_filename = file.replace(".csv", "") + "_avg_similarity"
         calculate_avg_similarity(file, similarity_output_filename)
         # count_output_filename = file.replace(".csv", "") + "_count"
