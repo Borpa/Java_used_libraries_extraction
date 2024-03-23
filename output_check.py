6699c796ac63db8aa7d8873558b18aa1d6f5c124
Nick Fed <fna.21@uni-dubna.ru>
2024-03-23 22:40:05 +0900

    updated functions

--- output_check.py
+++ output_check.py
@@ -1,4 +1,8 @@
-from run_pochi import check_classfile_local, check_classfile_size
+from run_pochi import (
+    check_classfile_local,
+    check_classfile_size,
+    check_classfile_only_size,
+)
 import pandas as pd
 import os
 import re
@@ -45,61 +49,77 @@ def check_class_single(project, project_ver, project_file, project_class, projec
         project, project_type, project_ver, project_file
     )
 
-    return check_classfile_local(project_file_path, project_class) is not False
+    return check_classfile_local(project_file_path, project_class)
+
+
+def check_class_size_single(
+    project, project_ver, project_file, project_class, project_type
+):
+    project_file_path = get_file_fullpath(
+        project, project_type, project_ver, project_file
+    )
+    return check_classfile_only_size(project_file_path, project_class)
 
 
 def main():
     chunksize = 1000000
     birthmark_dir = "G:/Study/phd_research/birthmarks/"
+    output_dir = "filtered/"
 
-    # birthmark_files = os.listdir(birthmark_dir)
-    # for birthmark_file in birthmark_files:
-    #    if not birthmark_file.endswith(".csv"):
-    #        continue
-    birthmark_file = "pochi-2.6.0_calculator_versions_output.csv"
-    header_check = True
-    with open(
-        birthmark_dir + birthmark_file.replace("_output", "_output_filtered"), "a"
-    ) as file:
-        project_type = get_project_type(birthmark_file)
-        for chunk in pd.read_csv(birthmark_dir + birthmark_file, chunksize=chunksize):
-            # mask = chunk.apply(
-            #    lambda x: check_class(x, project_type),
-            #    axis=1,
-            # )
-            # chunk = chunk[mask]
+    birthmark_files = os.listdir(birthmark_dir)
+    for birthmark_file in birthmark_files:
+        if not birthmark_file.endswith(".csv"):
+            continue
+        # birthmark_file = "pochi-2.6.0_calculator_versions_output.csv"
+        header_check = True
+        with open(
+            birthmark_dir
+            + output_dir
+            + birthmark_file.replace("_output", "_output_filtered"),
+            "a",
+            newline="",
+        ) as file:
+            project_type = get_project_type(birthmark_file)
+            for chunk in pd.read_csv(
+                birthmark_dir + birthmark_file, chunksize=chunksize
+            ):
+                # mask = chunk.apply(
+                #    lambda x: check_class(x, project_type),
+                #    axis=1,
+                # )
+                # chunk = chunk[mask]
 
-            i = 0
-            max_i = len(chunk)
-            while i < max_i:
-                cur_row = chunk.iloc[i]
-                check1 = check_class_single(
-                    cur_row.project1,
-                    cur_row.project1_ver,
-                    cur_row.project1_file,
-                    cur_row.class1,
-                    project_type,
-                )
-                if not check1:
-                    chunk = chunk[chunk.class1 != cur_row.class1]
-                    max_i = len(chunk)
-                    continue
+                i = 0
+                max_i = len(chunk)
+                while i < max_i:
+                    cur_row = chunk.iloc[i]
+                    check1 = check_class_single(
+                        cur_row.project1,
+                        cur_row.project1_ver,
+                        cur_row.project1_file,
+                        cur_row.class1,
+                        project_type,
+                    )
+                    if not check1:
+                        chunk = chunk[chunk.class1 != cur_row.class1]
+                        max_i = len(chunk)
+                        continue
 
-                check2 = check_class_single(
-                    cur_row.project2,
-                    cur_row.project2_ver,
-                    cur_row.project2_file,
-                    cur_row.class2,
-                    project_type,
-                )
-                if not check2:
-                    chunk = chunk[chunk.class2 != cur_row.class2]
-                    max_i = len(chunk)
-                    continue
-                i += 1
+                    check2 = check_class_single(
+                        cur_row.project2,
+                        cur_row.project2_ver,
+                        cur_row.project2_file,
+                        cur_row.class2,
+                        project_type,
+                    )
+                    if not check2:
+                        chunk = chunk[chunk.class2 != cur_row.class2]
+                        max_i = len(chunk)
+                        continue
+                    i += 1
 
-            chunk.to_csv(file, index=False, header=header_check)
-            header_check = False
+                chunk.to_csv(file, index=False, header=header_check)
+                header_check = False
 
 
 if __name__ == "__main__":
