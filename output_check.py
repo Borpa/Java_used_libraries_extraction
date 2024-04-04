1214add6ea224a967fb3d50f4d4da994b0b126a1
Nick Fed <fna.21@uni-dubna.ru>
2024-04-04 22:34:07 +0900

    filter by size loop

--- output_check.py
+++ output_check.py
@@ -164,8 +164,104 @@ def main():
             #    header_check = False
 
 
+def filter_by_size():
+    chunksize = 1000000
+    birthmark_dir = "G:/Study/phd_research/birthmarks/filtered/"
+    # birthmark_dir = "C:/Users/FedorovNikolay/source/VSCode_projects/Java_used_libraries_extraction/birthmarks/filtered"
+    output_dir = "filtered_by_size/"
+
+    birthmark_files = os.listdir(birthmark_dir)
+    #birthmark_files = [
+    #    "pochi-2.6.0_text_editor_distinct_output_filtered.csv",
+    #    "pochi-2.6.0_text_editor_versions_output_filtered.csv",
+    #]
+    for birthmark_file in birthmark_files:
+        if not birthmark_file.endswith(".csv"):
+            continue
+        # birthmark_file = "pochi-2.6.0_calculator_versions_output.csv"
+        header_check = True
+
+        project_type = get_project_type(birthmark_file)
+        for chunk in pd.read_csv(birthmark_dir + birthmark_file, chunksize=chunksize):
+            # mask = chunk.apply(
+            #    lambda x: check_class(x, project_type),
+            #    axis=1,
+            # )
+            # chunk = chunk[mask]
+
+            while len(chunk) > 0:
+                cur_row = chunk.iloc[0]
+                try:
+                    check1 = check_class_size_single(
+                        cur_row.project1,
+                        cur_row.project1_ver,
+                        cur_row.project1_file,
+                        cur_row.class1,
+                        project_type,
+                    )
+                    if not check1:
+                        chunk = chunk[chunk.class1 != cur_row.class1]
+                        continue
+                except TypeError:
+                    print(
+                        "TypeError for project {} {} with file {}, class: {}".format(
+                            cur_row.project1,
+                            cur_row.project1_ver,
+                            cur_row.project1_file,
+                            cur_row.class1,
+                        )
+                    )
+                    continue
+
+                try:
+                    check2 = check_class_size_single(
+                        cur_row.project2,
+                        cur_row.project2_ver,
+                        cur_row.project2_file,
+                        cur_row.class2,
+                        project_type,
+                    )
+                    if not check2:
+                        chunk = chunk[chunk.class2 != cur_row.class2]
+                        continue
+                except TypeError:
+                    print(
+                        "TypeError for project {} {} with file {}, class: {}".format(
+                            cur_row.project1,
+                            cur_row.project1_ver,
+                            cur_row.project1_file,
+                            cur_row.class1,
+                        )
+                    )
+                    continue
+
+                chunk_for_save = chunk[
+                    (chunk.class1 == cur_row.class1) & (chunk.class2 == cur_row.class2)
+                ]
+
+                if len(chunk_for_save) > 0:
+                    with open(
+                        birthmark_dir
+                        + output_dir
+                        + birthmark_file.replace("_filtered", "_filtered_size"),
+                        "a",
+                        newline="",
+                    ) as file:
+                        chunk_for_save.to_csv(file, index=False, header=header_check)
+                        header_check = False
+
+                    # chunk = pd.concat([chunk, chunk_for_save]).drop_duplicates(keep=False)
+                    chunk = chunk[
+                        ~(
+                            (chunk.class1 == cur_row.class1)
+                            & (chunk.class2 == cur_row.class2)
+                        )
+                    ]
+
+
 if __name__ == "__main__":
     start = time.time()
-    main()
+    # main()
+    filter_by_size()
     end = time.time()
     print(end - start)
