3494bccc9e67270ecb4009a55cd31e21b24b532c
nikfed <fedorov.nikolay.1110@gmail.com>
2024-07-18 11:59:08 +0900

    new methods templates

--- file_inspector.py
+++ file_inspector.py
@@ -1,10 +1,43 @@
 import os
 
 
-def inspect_dir(root_dir, output_file):
+def inspect_dir_by_num_of_lines(root_dir, output_file):
+    with open(output_file, mode="w") as f:
+        f.write("file,lines\n")
+
+    for path, subdirs, files in os.walk(root_dir):
+        for name in files:
+            if not name.endswith(".java"):
+                continue
+            filename = os.path.join(path, name)
+            lines = None
+
+            with open(output_file, mode="a") as f:
+                f.write(",".join([filename, lines]) + "\n")
+
+
+def inspect_dir_by_num_instruct(root_dir, output_file):
+    with open(output_file, mode="w") as f:
+        f.write("file,instructions count\n")
+
     for path, subdirs, files in os.walk(root_dir):
         for name in files:
             if not name.endswith(".class"):
+                continue
+            filename = os.path.join(path, name)
+            instruct_count = None
+
+            with open(output_file, mode="a") as f:
+                f.write(",".join([filename, instruct_count]) + "\n")
+
+
+def inspect_dir_by_filesize(root_dir, output_file, postfix=".class"):
+    with open(output_file, mode="w") as f:
+        f.write("file,size (B)\n")
+
+    for path, subdirs, files in os.walk(root_dir):
+        for name in files:
+            if not name.endswith(postfix):
                 continue
             filename = os.path.join(path, name)
             filesize = str(os.path.getsize(filename))
@@ -15,11 +48,10 @@ def inspect_dir(root_dir, output_file):
 
 def main():
     output_file = "project_files_size_data.csv"
-    with open(output_file, mode="w") as f:
-        f.write("file,size (B)\n")
+
     root_dir = "C:/Users/FedorovNikolay/source/Study/test_projects/current/"
 
-    inspect_dir(root_dir, output_file)
+    inspect_dir_by_filesize(root_dir, output_file)
 
 
 if __name__ == "__main__":
