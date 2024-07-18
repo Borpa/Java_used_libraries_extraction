69deea769957351e82e05ca51404115d61a47b82
nikfed <fedorov.nikolay.1110@gmail.com>
2024-07-18 11:19:29 +0900

    updated filesize extraction function

--- file_inspector.py
+++ file_inspector.py
@@ -4,20 +4,22 @@ import os
 def inspect_dir(root_dir, output_file):
     for path, subdirs, files in os.walk(root_dir):
         for name in files:
+            if not name.endswith(".class"):
+                continue
             filename = os.path.join(path, name)
-            filesize = os.path.getsize(filename)
+            filesize = str(os.path.getsize(filename))
 
             with open(output_file, mode="a") as f:
                 f.write(",".join([filename, filesize]) + "\n")
 
 
 def main():
-    output_file = "test.csv"
-
+    output_file = "project_files_size_data.csv"
     with open(output_file, mode="w") as f:
-        f.write("file,size\n")
+        f.write("file,size (B)\n")
+    root_dir = "C:/Users/FedorovNikolay/source/Study/test_projects/current/"
 
-    inspect_dir()
+    inspect_dir(root_dir, output_file)
 
 
 if __name__ == "__main__":
