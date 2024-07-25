19523d0d89fa4e3439e33df1ccded59743c533a7
nikfed <fedorov.nikolay.1110@gmail.com>
2024-07-25 11:15:14 +0900

    cleanup

--- birthmark_extraction.py
+++ birthmark_extraction.py
@@ -2,42 +2,25 @@ from multiprocessing import freeze_support
 
 import gc
 import run_pochi as pochi
-import run_stigmata as stigmata
-import time
 
 
 def main():
     gc.enable()
 
-    category_list = [
-        "/calculator/",
-        "/ai_app/",
-        "/terminal_app/",
-        "/emulator_environment/",
-        "/text_editor/",
-    ]
-    birthmark_list = ["uc", "fuc", "3-gram", "6-gram"]
-    for category in category_list:
-        pochi.extract_birthmarks(birthmark_list, category)
+    pochi.run_pochi_single_category("/calculator/", False, False)
+    pochi.run_pochi_single_category("/calculator/", True, False)
 
-    pochi.compare_external_birthmarks_distinct(category_list, birthmark_list)
-    pochi.compare_external_birthmarks_versions(category_list, birthmark_list)
-    
-    #for category in category_list:
-    #    start = time.time()
-    #    try:
-    #        pochi.run_pochi_single_category(category, True, False)
-    #    except Exception as e:
-    #        print(e)
-    #    end = time.time()
-    #    print("{} distinct runtime: ".format(category), end - start)
-    #    start = time.time()
-    #    try:
-    #        pochi.run_pochi_single_category(category, False, False)
-    #    except Exception as e:
-    #        print(e)
-    #    end = time.time()
-    #    print("{} versions runtime: ".format(category), end - start)
+    pochi.run_pochi_single_category("/text_editor/", True, False)
+    pochi.run_pochi_single_category("/text_editor/", False, False)
+
+    pochi.run_pochi_single_category("/terminal_app/", True, False)
+    pochi.run_pochi_single_category("/terminal_app/", False, False)
+
+    pochi.run_pochi_single_category("/emulator_environment/", True, False)
+    pochi.run_pochi_single_category("/emulator_environment/", False, False)
+
+    pochi.run_pochi_single_category("/ai_app/", True, False)
+    pochi.run_pochi_single_category("/ai_app/", False, False)
 
 
 if __name__ == "__main__":
